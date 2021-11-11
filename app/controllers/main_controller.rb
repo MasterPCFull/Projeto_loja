14b21c887cab94b16dbea62d94a5c1f57aa8ce45
MasterPCFull <italocarvalho100@gmail.com>
2021-11-11 12:20:10 -0300

    alert

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash[:notice] = "login realizado com sucesso"
+        flash[:alert] = "login ou senha incorretos"
     end
 end
